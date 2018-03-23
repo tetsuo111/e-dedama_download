e32cad116784b6bc47694ceea7653a648970e15a
tetsuo111 <tetsuochikazawa11@gmail.com>
2018-03-23 20:38:42 +0900

    hello

---
+++ php/twitter/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'kFtUEONdWLpexutxJ0AOIElcn');
+    define('CONSUMER_SECRET', 'Hi6f4ubJQVROP517nt5Uw0cLt0V0JZhezg8UcPjVu48ypTo98G');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '746408859959320576-J73IO59bpGZ9zcxkGRTT0Kz71VaDZSo');
+    define('ACCESS_SECRET', 'uidCryuhWdDeW53f23671NPhLMFXo8KjLtgGNCJn9yMjm');
+    
+    // Cache Settings
+    define('CACHE_ENABLED', false);
+    define('CACHE_LIFETIME', 3600); // in seconds
+    define('HASH_SALT', md5(dirname(__FILE__)));
